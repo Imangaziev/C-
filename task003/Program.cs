7d38a4443f2a5978384404bb11158edadc571f7b
Daniyar <lectorsez@gmail.com>
2023-02-22 15:25:25 +0600

    2й семинар по C#, ДЗ №6

---
+++ task003/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает
+// число и выдает, является ли число четным (делится ли оно на
+// два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
